94bdd5675fe5c5c959edeb5ce82c4bc6a06f345d
ThibautGobert <125729316+ThibautGobert@users.noreply.github.com>
2023-05-29 14:28:32 +0200

    update directory_separator in function .php

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
  */
 function getConfig(string $key):? string
 {
-    if(file_exists( __DIR__.'/config.php')) {
-        $config = require __DIR__.'/config.php';
+    if(file_exists( __DIR__.DIRECTORY_SEPARATOR.'config.php')) {
+        $config = require __DIR__.DIRECTORY_SEPARATOR.'config.php';
         if(is_array($config) && array_key_exists($key, $config)) {
             return $config[$key];
         }
@@ -22,7 +22,7 @@ function getConfig(string $key):? string
  */
 function getDirectories(): array
 {
-    $directories = array_filter(glob(getConfig('root_content') . '\*'), 'is_dir');
+    $directories = array_filter(glob(getConfig('root_content') . DIRECTORY_SEPARATOR.'*'), 'is_dir');
     $result = [];
     foreach ($directories as $directory) {
         $result[] = basename($directory, '/');
@@ -36,7 +36,7 @@ function getDirectories(): array
  */
 function getDirContents(string $directory): array
 {
-    $files = array_filter(glob($directory . '\*'), 'is_file');
+    $files = array_filter(glob($directory . DIRECTORY_SEPARATOR.'*'), 'is_file');
     $result = [];
     foreach ($files as $file) {
         $result[] = basename($file, '/');
